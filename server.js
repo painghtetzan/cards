aa3b9863d8de0f955706d1e06a0c317eff2e5d0c
painghtetzan <54760615+painghtetzan@users.noreply.github.com>
2026-01-19 22:20:37 +0800

    added edit and delete route

--- server.js
+++ server.js
@@ -47,3 +47,30 @@ app.post('/addcard',async(req,res)=>{
         res.status(500).send('error adding card')
     }
 })
+
+app.put('/editcard/:id',async(req,res)=>{
+    const {name,pic} = req.body
+    const id = req.params.id
+
+    try{
+        let connection = await sql.createConnection(dbConfig)
+        await connection.execute('UPDATE card SET card_name =?, card_pic=?  WHERE id=?',[name,pic,id])
+        res.status(202).send(`Updated card ${name} Successfully!`)
+    }catch(error){
+        console.error(error)
+        res.status(500).send('error updating card')
+    }
+})
+
+app.delete('/deletecard/:id',async(req,res)=>{
+    const id = req.params.id
+    
+    try{
+        let connection = await sql.createConnection(dbConfig)
+        await connection.execute('DELETE FROM card WHERE id =?',[id])
+        res.status(203).send('Successfully deleted')
+    }catch(error){
+        console.error(error)
+        res.status(500).send('error deleting card.')
+    }
+})
